ed1b5eca5415223031015e8614ec3bd3fae6506b
TinhoFZ <edupassosa18@gmail.com>
2025-06-04 09:00:55 -0300

    Atividade do dia realizada
    As atividades ainda estão presentes em ambos arquivos, src e aulas

---
+++ Atividades_Java/aula/atividade_2025_06_04/Hospital.java
@@ -0,0 +1,64 @@
+package atividade_2025_06_04;
+
+import java.util.Scanner;
+
+public class Hospital {
+
+// consultasPaciente vai armazenar o valor de todas as consultas do paciente
+public static String [] consultasPaciente = new String[255];
+public static int [] numeroConsultasMedico = new int [255];
+
+public static String cpfConsulta, medicoConsulta, data;
+// usados para contar o número atual de cadastros no sistema
+public static int dia, mes, ano;
+
+public static Scanner entrada = new Scanner(System.in);
+
+Medico medico = new Medico();
+	
+	public static void registrarConsulta() {
+		
+		System.out.println("CPF do paciente:");
+        cpfConsulta = entrada.nextLine();
+
+        System.out.println("Nome do médico:");
+        medicoConsulta = entrada.nextLine();
+        
+        System.out.println(Paciente.cpfPaciente[0]);
+    	System.out.println(Medico.nomeMedico[0]);
+        for (int i = 0; i < Paciente.numeroPacientes; i ++) {
+        	if (cpfConsulta.equalsIgnoreCase(Paciente.cpfPaciente[i]) && medicoConsulta.equalsIgnoreCase(Medico.nomeMedico[i])) {
+
+                System.out.println("Ano da consulta:");
+                ano = entrada.nextInt();
+                System.out.println("Mês da consulta:");
+                mes = entrada.nextInt();
+                System.out.println("Dia da consulta:");
+                dia = entrada.nextInt();
+                
+                entrada.nextLine();
+                
+                data = dia + "/" + mes + "/" + ano;
+                
+                // Armazenando a data de todas as consultas no mesmo número de array do paciente
+                consultasPaciente[i] = consultasPaciente[i] + "\n" + data;
+                
+                System.out.println("Consulta registrada.");
+        	}
+        }
+        Main.main(null);
+	}
+	
+	public static void consultasPaciente() {
+		System.out.println("CPF do paciente:");
+        
+        cpfConsulta = entrada.nextLine();
+        
+        for (int i = 0; i < Paciente.numeroPacientes; i ++) {
+        	if (cpfConsulta.equalsIgnoreCase(Paciente.cpfPaciente[i])) {
+        		System.out.println(consultasPaciente[i]);
+        		Main.main(null);
+        	}
+        }
+	}
+}
---
+++ Atividades_Java/aula/atividade_2025_06_04/Paciente.java
@@ -0,0 +1,24 @@
+package atividade_2025_06_04;
+
+import java.util.Scanner;
+
+public class Paciente {
+
+public static Scanner entrada = new Scanner(System.in);
+
+public static String[] nomePaciente = new String [255], cpfPaciente = new String [255];
+public static int numeroPacientes = 0;
+
+	public static void cadastrarPaciente() {
+		
+		System.out.println("Nome do paciente:");
+        nomePaciente[numeroPacientes] = entrada.nextLine();
+
+        System.out.println("CPF:");
+        cpfPaciente[numeroPacientes] = entrada.nextLine();
+        numeroPacientes ++;
+        
+        System.out.println("Paciente cadastrado.");
+        Main.main(null);
+	}
+}
