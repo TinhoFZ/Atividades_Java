2a93bc66e407abb84e44f9e51781b02c58eaeeca
Eduardo Passos <edupassosa18@gmail.com>
2025-07-01 17:42:28 -0300

    2 novas atividades realizadas

--- atividadesJava/pessoal/hashmap/Main.java
+++ atividadesJava/pessoal/hashmap/Main.java
@@ -4,33 +4,34 @@ import java.util.Scanner;
 
 public class Main {
 
-	public Scanner input = new Scanner(System.in);
+	public static Scanner input = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		Atv1 atv1 = new Atv1();
 		Atv2 atv2 = new Atv2();
 		Atv3 atv3 = new Atv3();
+		Atv4 atv4 = new Atv4();
+		Atv5 atv5 = new Atv5();
 		
-		System.out.println("Que atividade deseja realizar?"
-				+ "[1]"
-				+ "[2]"
-				+ "[3]");
+		System.out.println("Que atividade deseja visualizar? [1] [2] [3] [4] [5]");
 		int escolha = input.nextInt();
 		
 		switch (escolha) {
 		case 1:
-			
-		}
-//		
-//		atv1.rodar();
-		
-//		
-//		atv2.rodar();
-		
-//		
-//		atv3.rodar();
-
-		
+			atv1.rodar();
+			break;
+		case 2:
+			atv2.rodar();
+			break;
+		case 3:
+			atv3.rodar();
+			break;
+		case 4:
+			atv4.rodar();
+			break;
+		case 5:
+			atv5.rodar();
+			break;
+		}		
 	}
-
 }
